8447869d04beb5f31f2450784cbd6f8423df27d2
ABSanthosh <a.b.santhosh02@gmail.com>
2022-02-20 00:17:30 +0530

    feat(Component): Card without image of project

---
+++ src/Components/CardWithoutImage/CardWithoutImage.tsx
@@ -0,0 +1,12 @@
+import "./CardWithoutImage.scss";
+
+export interface ICardWithoutImageProps {
+}
+
+export function CardWithoutImage (props: ICardWithoutImageProps) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
