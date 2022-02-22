abad08f3e57f0047a2337061a5c2d68b8c27ff49
ABSanthosh <a.b.santhosh02@gmail.com>
2022-02-22 19:04:01 +0530

    feat(API): Check if repo exists

--- src/operations/githubApi.ts
+++ src/operations/githubApi.ts
@@ -3,3 +3,10 @@ import axios from "axios";
 export const getRepo = async (owner: string, repo: string) => {
   return await axios.get(`https://api.github.com/repos/${owner}/${repo}`);
 };
+
+export const isGitHubRepo = async (owner: string, repo: string) => {
+  const googleProxyURL =
+    "https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=";
+  const url = encodeURIComponent(`https://github.com/${owner}/${repo}`);
+  return await axios.get(`${googleProxyURL}${url}`);
+};
